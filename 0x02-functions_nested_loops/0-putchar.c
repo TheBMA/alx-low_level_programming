98b4f3ce0d18cfe1cb0361ef5fd9bec979df62b3
Mohamed Ali <benrochd.m.ali@gmail.com>
2023-07-18 11:54:17 -0700

    added 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ *
+ * Description: a program that prints _putchar, followed by a new line.
+ * Not allowed to use the standard library.
+ * Any use of functions like printf, puts, etc… is forbidden.
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}

58aef028c016738866fe60617e41a9dcde71a112
Mohamed Ali <benrochd.m.ali@gmail.com>
2023-07-19 08:40:53 -0700

    0 fixed

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,36 +1,3 @@
 #include <unistd.h>
-#include <stdio.h>
-
-/**
- * main - main block
- *
- * Description: header file for 0-putchar.c, 1-alphabet.c
- *
- * Return: 0
- */
-
-static inline int _putchar(void)
-{
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
-
-	return (0);
-}
-
-static inline void print_alphabet(void)
-{
-	int x;
-
-	for (x = 'a'; x <= 'z'; x++)
-		putchar (x);
-
-	putchar ('\n');
-
-}
+int _putchar(char c);
+void print_alphabet(void);

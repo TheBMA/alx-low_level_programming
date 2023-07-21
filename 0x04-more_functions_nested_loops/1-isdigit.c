25a41ba7a15a16f1ba8955fd6e85a01abf884ac9
Mohamed Ali <benrochd.m.ali@gmail.com>
2023-07-21 16:30:03 -0700

    betty description

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,12 +3,14 @@
 /**
  * _isdigit - a function that checks for a digit (0 through 9).
  * Prototype: int _isdigit(int c);
- *
+ * @c: character to check.
  * Return: 1 if c is a digit, 0 otherwise.
  */
 
 int _isdigit(int c)
 {
+	int c;
+
 	if (c >= '0' && c <= '9')
 		return (1);
 	else

b0d5d352ae6512ab3ca9f92ffaf4aebde258c3a2
Mohamed Ali <benrochd.m.ali@gmail.com>
2023-07-25 17:47:26 -0700

    new line no matter what

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,15 +13,12 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	if (n > 0)
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
-		{
-			printf("%d", a[i]);
-			if (i + 1 < n)
-				printf(", ");
-			else
-				printf("\n");
-		}
+		printf("%d", a[i]);
+		if (i + 1 < n)
+			printf(", ");
 	}
+
+	printf("\n");
 }

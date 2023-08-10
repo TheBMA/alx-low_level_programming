801dac3154011fe75f5c8afe6216f3dac42048f7
Mohamed Ali <benrochd.m.ali@gmail.com>
2023-08-09 17:45:50 -0700

    memory has never been so free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -30,7 +30,7 @@ int **alloc_grid(int width, int height)
 	{
 		grid[i] = malloc(height * sizeof(int));
 
-		if (grid == NULL)
+		if (grid[i] == NULL)
 		{
 			for (j = 0; j < i; j++)
 				free(grid[j]);

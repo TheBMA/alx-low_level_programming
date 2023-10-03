76f77aa0ba8b101c43be00b2ddeb80bb467a9b1d
TheBMA <benrochd.m.ali@gmail.com>
2023-10-02 18:54:37 -0700

    Check 7

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,22 +15,19 @@ int main(int argc, char *argv[])
 	int sum = 0;
 	int i;
 
-	if (argc == 1)
+	if (argc > 1)
 	{
-		printf("0\n");
-		return (0);
-	}
-
-	for (i = 1; i < argc; i++)
-	{
-		if (atoi(argv[i]) <= 0)
+		for (i = 1; i < argc; i++)
 		{
-			printf("Error\n");
-			return (1);
-		}
-		else
-		{
-			sum += atoi(argv[i]);
+			if (atoi(argv[i]) <= 0)
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+			{
+				sum += atoi(argv[i]);
+			}
 		}
 	}
 

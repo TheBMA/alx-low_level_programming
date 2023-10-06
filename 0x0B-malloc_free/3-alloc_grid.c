814b23a5fc0f1be660930c0968082cfd6314f3fe
TheBMA <benrochd.m.ali@gmail.com>
2023-10-05 19:06:29 -0700

    free memory

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,13 +21,22 @@ int **alloc_grid(int width, int height)
 	grid = (int **) malloc(height * sizeof(int));
 
 	if (grid == NULL)
+	{
+		free(grid);
 		return (NULL);
+	}
 
 	for (i = 0; i < height; i++)
 	{
 		grid[i] = (int *) malloc(width * sizeof(int));
 		if (grid[i] == NULL)
+		{
+			for (; i >= 0; i--)
+				free(grid[i]);
+
+			free(grid);
 			return (NULL);
+		}
 
 		for (j = 0; j < width; j++)
 			grid[i][j] = 0;

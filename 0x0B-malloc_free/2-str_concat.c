00424bf3237e545d7712d2f4851c72a37314c868
TheBMA <benrochd.m.ali@gmail.com>
2023-10-05 17:35:13 -0700

    Task 2 2nd half

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,20 +13,42 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int i, len1, len2, len3;
+	int i, j, len1, len2, len3;
 	char *s3;
 
-	for (len1 = 0; s1[len1] != '\0'; len1++)
+	if (s1 == NULL)
+		len1 = 0;
+	else
+	{
+		for (len1 = 0; s1[len1] != '\0'; len1++)
 		;
+	}
 
-	for (len2 = 0; s2[len2] != '\0'; len2++)
+	if (s2 == NULL)
+		len2 = 0;
+	else
+	{
+		for (len2 = 0; s2[len2] != '\0'; len2++)
 		;
+	}
 
 	len3 = len1 + len2;
 
 	s3 = (char *) malloc(len3 * sizeof(char) + 1);
 
-	if (s3 = NULL)
+	if (s3 == NULL)
 		return (NULL);
 
+	for (i = 0; i < len1; i++)
+		s3[i] = s1[i];
+
+	for (j = 0; j < len2; j++)
+	{
+		s3[i] = s2[j];
+		i++;
+	}
+
+	s3[i] = '\0';
+
+	return (s3);
 }

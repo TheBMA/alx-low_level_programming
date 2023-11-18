3e12e1151d191d99071a8b19a9a1a01e7dade48e
TheBMA <benrochd.m.ali@gmail.com>
2023-11-18 03:42:49 -0800

    Betty style + the 0 case

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,7 +13,14 @@ void print_times_table(int n)
 {
 	int i, j;
 
-	if (n <= 15 && n >= 0)
+	if (n == 0)
+	{
+		_putchar('0');
+		_putchar('\n');
+		return;
+	}
+
+	if (n <= 15 && n > 0)
 	{
 		for (i = 0; i <= n; i++)
 		{
@@ -26,31 +33,45 @@ void print_times_table(int n)
 				}
 				else
 				{
-						_putchar(' ');
-					if ((i * j) < 10)
-					{
-						_putchar(' ');
-						_putchar(' ');
-						_putchar((i * j) + '0');
-					}
-					else if ((i * j) < 100)
-					{
-						_putchar(' ');
-						_putchar((((i * j) / 10) % 10) + '0');
-						_putchar(((i * j) % 10) + '0');
-					}
-					else
-					{
-						_putchar((((i * j) / 100) % 10) + '0');
-						_putchar((((i * j) / 10) % 10) + '0');
-						_putchar(((i * j) % 10) + '0');
-					}
-					if (j == n)
-						_putchar('\n');
-					else
-						_putchar(',');
+					_putchar(' ');
+					timez_table(i, j);
+				if (j == n)
+					_putchar('\n');
+				else
+					_putchar(',');
 				}
 			}
 		}
+	}
+}
+
+/**
+ * timez_table - function that completes print_times_table
+ * to keep the function print_times_table under 40 lines (for Betty style)
+ * Prototype: void timez_table(int i, int j);
+ * @i: an integer.
+ * @j: an integer.
+ * Return: void.
+ */
+
+void timez_table(int i, int j)
+{
+	if ((i * j) < 10)
+	{
+		_putchar(' ');
+		_putchar(' ');
+		_putchar((i * j) + '0');
+	}
+	else if ((i * j) < 100)
+	{
+		_putchar(' ');
+		_putchar((((i * j) / 10) % 10) + '0');
+		_putchar(((i * j) % 10) + '0');
+	}
+	else
+	{
+		_putchar((((i * j) / 100) % 10) + '0');
+		_putchar((((i * j) / 10) % 10) + '0');
+		_putchar(((i * j) % 10) + '0');
 	}
 }

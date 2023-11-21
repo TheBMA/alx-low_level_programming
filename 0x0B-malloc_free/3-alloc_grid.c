1d414467d570e8b5f8845b8354365a2ab2a5acb5
TheBMA <benrochd.m.ali@gmail.com>
2023-11-21 07:49:17 -0800

    Revert changes

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -31,7 +31,7 @@ int **alloc_grid(int width, int height)
 		ar[i] = malloc(sizeof(int) * height);
 		if (ar[i] == NULL)
 		{
-			for (j = 0; j <= i; j++)
+			for (; i >= 0; i--)
 				free(ar[i]);
 			free(ar);
 			return (NULL);

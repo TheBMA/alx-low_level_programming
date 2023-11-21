8167ee5558f4facf41c798e5fb5477f441476efe
TheBMA <benrochd.m.ali@gmail.com>
2023-11-21 12:10:28 -0800

    Fixed amount of memory allocated

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -19,7 +19,7 @@ char *argstostr(int ac, char **av)
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	for (i = 1, len = 0; i < ac; i++)
+	for (i = 0, len = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
 		{

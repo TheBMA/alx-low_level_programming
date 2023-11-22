4cc9ca63429869075f758667e128cccade2ab730
TheBMA <benrochd.m.ali@gmail.com>
2023-11-22 10:17:01 -0800

    Task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,11 +2,11 @@
 
 /**
  * print_number - a function that prints an integer.
- * only use _putchar function to print,
  * not allowed to use long, arrays or pointers.
  * not allowed to hard-code special values.
- * @n: integer to print.
  * Prototype: void print_number(int n);
+ * @n: integer to print.
+ * Return: void.
  */
 
 void print_number(int n)
@@ -17,10 +17,8 @@ void print_number(int n)
 		n = -n;
 	}
 
-	if (n / 10)
-	{
+	if ((n / 10) != 0)
 		print_number(n / 10);
-	}
 
-	_putchar (n % 10 + '0');
+	_putchar ((n % 10) + '0');
 }

4f0f3602f23cc3d0c08b89501b7e957908b80dc1
TheBMA <benrochd.m.ali@gmail.com>
2023-11-28 15:18:15 -0800

    Edited init_dog and added declarations

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,7 +8,6 @@
  * @name: a string of characters.
  * @age: a float.
  * @owner: a string of characters.
- *
  */
 
 typedef struct dog
@@ -19,4 +18,7 @@ typedef struct dog
 } dog;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif

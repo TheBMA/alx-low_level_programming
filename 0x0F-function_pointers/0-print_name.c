59407749f38d5e3b9fc7b3ef6ea79e3350ca486f
TheBMA <benrochd.m.ali@gmail.com>
2023-12-01 10:02:19 -0800

    Task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,27 +1,4 @@
 #include "function_pointers.h"
-#include <stddef.h>
-
-/**
- * array_iterator - a function that executes a function given
- * as a parameter on each element of an array.
- * Prototype:
- * void array_iterator(int *array, size_t size, void (*action)(int));
- * @array: a pointer to an integer.
- * @size:
- * @action: pointer to a function that
- * Return: void.
- */
-
-void array_iterator(int *array, size_t size, void (*action)(int))
-{
-
-}
-
-
-
-
-
-#include "function_pointers.h"
 
 /**
  * print_name - a function that prints a name.

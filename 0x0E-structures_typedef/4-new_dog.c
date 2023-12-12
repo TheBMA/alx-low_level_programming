e0c69e537ff06ea62f4b41cf1b09fd8f02414ea5
TheBMA <benrochd.m.ali@gmail.com>
2023-12-12 02:08:31 -0800

    updated function that duplicates strings

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,14 +12,46 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newdog;
+	char *newname, *newowner;
+	int i = 0;
 
 	newdog = malloc(sizeof(dog_t));
 	if (newdog == NULL)
 		return (NULL);
 
-	newdog->name = name;
+	newname = malloc(sizeof(name));
+	if (newname == NULL)
+	{
+		free(newdog);
+		return (NULL);
+	}
+
+	newowner = malloc(sizeof(owner));
+	if (newowner == NULL)
+	{
+		free(newdog);
+		free(newname);
+		return (NULL);
+	}
+
+	while (name[i] != '\0')
+	{
+		newname[i] = name[i];
+		i++;
+	}
+	newname[i] = '\0';
+
+	i = 0;
+	while (owner[i] != '\0')
+	{
+		newowner[i] = owner[i];
+		i++;
+	}
+	newowner[i] = '\0';
+
+	newdog->name = newname;
 	newdog->age = age;
-	newdog->owner = owner;
+	newdog->owner = newowner;
 
 	return (newdog);
 }

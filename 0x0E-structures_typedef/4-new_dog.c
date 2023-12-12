6dcf4d70b1e93268210a7ae2ce7702cbd65a1d99
TheBMA <benrochd.m.ali@gmail.com>
2023-12-12 02:20:53 -0800

    malloced newdog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,20 +13,25 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newdog;
 	char *newname, *newowner;
-	int i = 0;
+	int i, namelen = 0, ownerlen = 0;
+
+	for (i = 0; name[i] != '\0'; i++)
+		namelen++;
+	for (i = 0; owner[i] != '\0'; i++)
+		ownerlen++;
 
 	newdog = malloc(sizeof(dog_t));
 	if (newdog == NULL)
 		return (NULL);
 
-	newname = malloc(sizeof(name));
+	newname = malloc(namelen + 1);
 	if (newname == NULL)
 	{
 		free(newdog);
 		return (NULL);
 	}
 
-	newowner = malloc(sizeof(owner));
+	newowner = malloc(namelen + 1);
 	if (newowner == NULL)
 	{
 		free(newdog);
@@ -34,24 +39,15 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	while (name[i] != '\0')
-	{
+	for (i = 0; name[i] != '\0'; i++)
 		newname[i] = name[i];
-		i++;
-	}
 	newname[i] = '\0';
-
-	i = 0;
-	while (owner[i] != '\0')
-	{
+	for (i = 0; owner[i] != '\0'; i++)
 		newowner[i] = owner[i];
-		i++;
-	}
 	newowner[i] = '\0';
 
 	newdog->name = newname;
 	newdog->age = age;
 	newdog->owner = newowner;
-
 	return (newdog);
 }

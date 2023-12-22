da98b7f2cb4e5e3610cb1345b9d4088caa34bbc8
TheBMA <benrochd.m.ali@gmail.com>
2023-12-22 13:08:26 -0800

    Task 3, over 40 lines, wrong permissions

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,59 @@
+#include "main.h"
+
+/**
+ * main - a program that copies the content of a file to another file.
+ * @argc: argument count.
+ * @argv: argument vector.
+ * Return: 0 if successful.
+ */
+
+int main(int argc, char *argv[])
+{
+	int fromDescriptor, toDescriptor, bytesRead, bytesWritten;
+	char *file_from, *file_to;
+	char buffer[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	file_from = argv[1];
+	file_to = argv[2];
+	fromDescriptor = open(file_from, O_RDONLY);
+	toDescriptor = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+
+	while ((bytesRead = read(fromDescriptor, buffer, sizeof(buffer))) > 0)
+	{
+		if (fromDescriptor == -1 || bytesRead == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			close(fromDescriptor);
+			exit(98);
+		}
+
+		bytesWritten = write(toDescriptor, buffer, bytesRead);
+
+		if (toDescriptor == -1 || bytesWritten == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(fromDescriptor);
+			close(toDescriptor);
+			exit(99);
+		}
+	}
+
+	if (close(fromDescriptor) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fromDescriptor);
+		exit(100);
+	}
+	if (close(toDescriptor) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", toDescriptor);
+		exit(100);
+	}
+
+	return (0);
+}

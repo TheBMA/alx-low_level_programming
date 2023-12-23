e1c4b26ea4b1202b4c22d43b8cfdeb13121a7df4
TheBMA <benrochd.m.ali@gmail.com>
2023-12-23 05:28:56 -0800

    output lengh fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,7 +26,7 @@ int create_file(const char *filename, char *text_content)
 		for (i = 0; text_content[i] != '\0'; i++)
 			text_lengh++;
 
-		bytesWritten = write(fileDescriptor, text_content, text_lengh + 1);
+		bytesWritten = write(fileDescriptor, text_content, text_lengh);
 
 		if (bytesWritten == (-1))
 			return (-1);
